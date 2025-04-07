13fe28c70f28dd34d4fe01066ce502bc836df60a
hongwoomin <hongwoomin02@gmail.com>
2025-04-07 15:57:58 +0900

    Type: feat
    하단에 디바이스목록페이지와 트리거목록페이지를 이동가능하게하는 네비게이션바를 구현.

--- client/App.tsx
+++ client/App.tsx
@@ -1,11 +1,54 @@
 import React from "react";
-import { View } from "react-native";
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { View,StyleSheet} from "react-native";
 import DevicePage from "./src/screens/DevicePage"; 
+
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
-    <DevicePage/>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="디바이스 목록 페이지"
+        screenOptions={{
+          headerShown: false,
+          tabBarStyle: styles.tabBarStyle,
+          tabBarLabelStyle: styles.tabBarLabelStyle,
+          tabBarActiveTintColor: "#fff",
+          tabBarIcon: () => null,
+          tabBarBackground: () => (
+              <View style={styles.divider} />
+          ),
+        }}
+      >
+        <Tab.Screen name="디바이스 목록 페이지" component={DevicePage} />
+        {/* <Tab.Screen name="트리거 목록 페이지" component={TriggerPage} /> */}
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  tabBarStyle: {
+    height: 90,
+    backgroundColor: "#000", 
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    position: "absolute",
+  },
+  tabBarLabelStyle: {
+    fontSize: 17, 
+    fontWeight: "bold",
+  },
+  divider: {
+    position: "absolute",
+    width: 1.5,
+    height: 90, 
+    backgroundColor: "#fff", 
+    left: "50%",
+  },
+});
 
 export default App;
