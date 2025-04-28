4a2fc4db008a78c673921a5e4decaf78dd5db978
hongwoomin <hongwoomin02@gmail.com>
2025-04-28 16:44:17 +0900

    Type: feat
    현재 디바이스박스가 여러개 추가되면
    네비게이션바가 마지막 디바이스박스를 가리게 됨
    -> 네비게이션바높이 +10만큼 margin을 둬서 해결

--- client/src/screens/DevicePage/DPage.js
+++ client/src/screens/DevicePage/DPage.js
@@ -1,20 +1,27 @@
 import React from "react";
-import { ScrollView, View, Text, StyleSheet } from "react-native";
+import { ScrollView, View } from "react-native";
+import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
 import Background from "./components/Background.js";
 import { AppText, styles as appTextStyles } from "./components/AppText.js";
 import DeviceBox from "./components/DeviceBox.js";
+
 const DPage = () => {
+  const tabBarHeight = useBottomTabBarHeight();
+
   return (
     <View style={{ flex: 1 }}>
       <Background />
-      <ScrollView>
-        <View>
-          <AppText style={appTextStyles.text1}>딸깍</AppText>
-          <AppText style={appTextStyles.text2}>쉽고 편한 기기관리</AppText>
-          <AppText style={appTextStyles.text3}>My Devices</AppText>
-          <AppText style={appTextStyles.text4}>내 기기 My Devices</AppText>
-          <DeviceBox />
-        </View>
+      <ScrollView
+        contentContainerStyle={{
+          paddingBottom: tabBarHeight + 10, 
+        }}
+        showsVerticalScrollIndicator={false}
+      >
+        <AppText style={appTextStyles.text1}>딸깍</AppText>
+        <AppText style={appTextStyles.text2}>쉽고 편한 기기관리</AppText>
+        <AppText style={appTextStyles.text3}>My Devices</AppText>
+        <AppText style={appTextStyles.text4}>내 기기 My Devices</AppText>
+        <DeviceBox />
       </ScrollView>
     </View>
   );
