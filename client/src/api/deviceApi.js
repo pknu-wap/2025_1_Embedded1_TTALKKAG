4743d99eaff80e6de16fecab66b80d050e8591d9
hongwoomin <hongwoomin02@gmail.com>
2025-04-30 18:07:32 +0900

    Type: feat
    1. Api 통신을 위한 deviceApi.js를 만듦
      -> 통신에 관한 모듈 분리를 위해
    2. .env 파일을 추가: 서버ip주소 추가
       -> gitignore
    3. registerDevice로 앱시작하자마자 기기등록 post 보내기
     /mqtt/new-device
      -> 브로커 켜져있을때 통신 테스트 해봄, 현재는 꺼져있어서 에러메시지
    4. 디바이스목록페이지에 기기목록 불러오기 api를 통해서 불러온 기기들 디바이스박스에 표시
     -> 현재는 버튼클리커 디바이스박스만 표시됨 추후 다이얼은 디바이스박스가 토글되는 디바이스박스로 나오게 구현 해둠
    5. 전원버튼 누르면
     /mqtt/${deviceId}/press  요청이 가게해둠

---
+++ client/src/api/deviceApi.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import Config from 'react-native-config';
+
+const BASE_URL = Config.API_URL;
+
+// 기기 등록 (POST)
+export const registerDevice = async () => {
+  const url = `${BASE_URL}/mqtt/new-device`;
+  return axios.post(url, {
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    }
+  });
+};
+
+// 2) 기기 목록 불러오기 (GET)
+export const fetchDeviceList = async () => {
+  const url = `${BASE_URL}/device/list`;
+  return axios.get(url, {
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    }
+  });
+};
+// 3) 전원버튼 누르기 (POST)
+export const pressDevice = async (deviceId) => {
+  const url = `${BASE_URL}/mqtt/${deviceId}/press`;
+  return axios.post(url, null, {
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    }
+  });
+};
--- client/src/screens/DevicePage/DPage.js
+++ client/src/screens/DevicePage/DPage.js
@@ -1,27 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ScrollView, View } from "react-native";
 import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
 import Background from "./components/Background.js";
 import { AppText, styles as appTextStyles } from "./components/AppText.js";
 import DeviceBox from "./components/DeviceBox.js";
+import { fetchDeviceList } from "../../api/deviceApi"; 
 
 const DPage = () => {
-  const tabBarHeight = useBottomTabBarHeight();
+  const tabBarHeight = useBottomTabBarHeight(); // 하단 탭바 높이를 가져와서 ScrollView 패딩에 반영
+  const [deviceList, setDeviceList] = useState([]); // 기기 목록을 저장할 state
+
+  // 기기 목록 불러오는
+  const loadDevices = async () => {
+    try {
+      const response = await fetchDeviceList();  // 서버로부터 기기목록 요청
+      console.log("기기 목록 로드 성공:", response.data);
+      // buttons와 dials를 하나의 배열로 합쳐 저장 (각 항목에 type 속성 추가)
+      // DeviceBox에서 type에 따라 다른 UI를 구성할 수 있게 하기 위함
+      setDeviceList([
+        ...response.data.buttons.map((b) => ({ ...b, type: "button" })),
+        ...response.data.dials.map((d) => ({ ...d, type: "dial" }))
+      ]);
+    } catch (error) {
+        console.log(error.message);
+      }
+  };
+
+  useEffect(() => {
+    loadDevices();
+  }, []);
 
   return (
     <View style={{ flex: 1 }}>
       <Background />
+      {/* 기기 리스트를 스크롤 가능한 영역에 렌더링 탭바 가리지 않도록 하단 패딩 추가 */}
       <ScrollView
-        contentContainerStyle={{
-          paddingBottom: tabBarHeight + 10, 
-        }}
+        contentContainerStyle={{ paddingBottom: tabBarHeight + 10 }}
         showsVerticalScrollIndicator={false}
       >
         <AppText style={appTextStyles.text1}>딸깍</AppText>
         <AppText style={appTextStyles.text2}>쉽고 편한 기기관리</AppText>
         <AppText style={appTextStyles.text3}>My Devices</AppText>
         <AppText style={appTextStyles.text4}>내 기기 My Devices</AppText>
-        <DeviceBox />
+
+        {/* 기기 목록을 map으로 순회하여 DeviceBox 컴포넌트로 렌더링 */}
+        {deviceList.map((device) => (
+          <DeviceBox
+            key={device.id}
+            id={device.id}
+            name={device.name}
+            type={device.type} // button 또는 dial
+          />
+        ))}
       </ScrollView>
     </View>
   );
