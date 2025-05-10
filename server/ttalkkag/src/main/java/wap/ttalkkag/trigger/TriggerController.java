bd13a3cb613455bad41e6991197b7c9cfcef6b43
kokeunho <kokangsik65@naver.com>
2025-05-11 02:37:43 +0900

    Revert "63-feat-트리거-내에서-특정-기기-활성화-비활성화"
    This reverts commit 642c62ea082b4b4474897dc75f6377367abd1e94.

--- server/ttalkkag/src/main/java/wap/ttalkkag/trigger/TriggerController.java
+++ server/ttalkkag/src/main/java/wap/ttalkkag/trigger/TriggerController.java
@@ -2,7 +2,10 @@ package wap.ttalkkag.trigger;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import wap.ttalkkag.domain.Door;
 import wap.ttalkkag.domain.Trigger;
 
@@ -13,11 +16,10 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TriggerController {
     private final TriggerService triggerService;
+    private final Long userId = 1L;
 
-    /*user의 door_sensor 목록*/
     @GetMapping("/list")
     public ResponseEntity<List<Door>> getTriggerList() {
-        Long userId = 1L;
         List<Door> doors = triggerService.getTriggers(userId);
         return ResponseEntity.ok(doors);
     }
@@ -27,21 +29,9 @@ public class TriggerController {
     * 우선 등록된 모든 디바이스 목록을 불러온 후
     * 해당 api로 트리거에 속한 디바이스들의 타입과 id를 받아서
     * 위 모든 디바이스 목록 중 표시*/
-    @GetMapping("/{door_id}/active_devices")
-    public ResponseEntity<List<Trigger>> getActiveDevices(@PathVariable("door_id") Long doorId) {
-        List<Trigger> activeDevices = triggerService.getActiveDevices(doorId);
+    @GetMapping("/{trigger_id}/active_devices")
+    public ResponseEntity<List<Trigger>> getActiveDevices(@PathVariable("trigger_id") Long triggerId) {
+        List<Trigger> activeDevices = triggerService.getActiveDevices(triggerId);
         return ResponseEntity.ok(activeDevices);
-    }
-    /*트리거에 기기 추가(활성화) */
-    @PostMapping("/activate")
-    public ResponseEntity<Void> activateDevice(@RequestBody ActivateDeviceDTO request) {
-        triggerService.activateDevice(request);
-        return ResponseEntity.ok().build();
-    }
-    /*트리거에서 기기 제외(비활성화)*/
-    @DeleteMapping("/deactivate")
-    public ResponseEntity<Void> deactivateDevice(@RequestBody ActivateDeviceDTO request) {
-        triggerService.deactivateDevice(request);
-        return ResponseEntity.ok().build();
     }
 }
